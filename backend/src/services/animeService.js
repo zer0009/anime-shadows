8d23f6be6859fd400789e08113ce83d0ac64bc50
Ahmed <egydark999@gmail.com>
2024-07-29 23:23:00 +0300

    Fix Some Issues

--- backend/src/services/animeService.js
+++ backend/src/services/animeService.js
@@ -18,7 +18,7 @@ const validateEntityById = async (Model, id, entityName, file) => {
   return entity;
 };
 
-const createAnime = async ({ title, description, seasonId, myAnimeListUrl, typeId, genres, numberOfEpisodes, source, duration, status, airingDate, pictureUrl }) => {
+const createAnime = async ({ title, description, seasonId, myAnimeListUrl, typeId, genres, numberOfEpisodes, source, duration, status, airingDate, pictureUrl,subTitle, studio }) => {
   if (!typeId || !seasonId) {
     throw new Error('Type ID and Season ID are required');
   }
