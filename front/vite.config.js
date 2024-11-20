c721b46fca2a39df31b7aa22e43a28607ec4587a
zero009 <egydark999@gmail.com>
2024-11-20 06:17:31 +0200

    impove the website loading - 3

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,32 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import sentryVitePlugin from '@sentry/vite-plugin' // Optional: If using Sentry for error tracking
 import { resolve } from 'path'
 import dotenv from 'dotenv'
 
 // Load environment variables from .env file
 dotenv.config()
 
-// Destructure necessary environment variables
-const {
-  SENTRY_AUTH_TOKEN,
-  SENTRY_ORG,
-  SENTRY_PROJECT,
-  VITE_RELEASE, // Ensure this is set during deployment
-} = process.env
-
 export default defineConfig({
   plugins: [
     react(),
-    // Optional: Integrate Sentry Vite plugin for error tracking with source maps
-    sentryVitePlugin({
-      org: SENTRY_ORG || '',
-      project: SENTRY_PROJECT || '',
-      authToken: SENTRY_AUTH_TOKEN || '',
-      release: VITE_RELEASE || 'anime-shadows@1.0.0',
-      include: './dist', // Directory with build assets
-      ignore: ['node_modules', 'vite.config.js'], // Files to ignore
-    }),
   ],
   build: {
     sourcemap: true, // Enable source map generation for debugging
