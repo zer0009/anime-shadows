bc66482c02211a53eabeebccb1787f7c1bc1291f
zero009 <egydark999@gmail.com>
2024-11-20 06:02:54 +0200

    impove the website loading - 1

--- front/vite.config.js
+++ front/vite.config.js
@@ -5,30 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-      sourcemap: true, // Enable source map generation
-      outDir: 'dist',
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, 'index.html'),
-        },
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              // Split vendor code into separate chunks for better caching
-              return id.toString().split('node_modules/')[1].split('/')[0].toString()
-            }
-          },
-        },
+    sourcemap: true, // Enable source map generation
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
       },
+    },
   },
   publicDir: 'public',
-  optimizeDeps: {
-    include: [
-      // Pre-bundle dependencies to improve performance
-      'react',
-      'react-dom',
-      'react-router-dom',
-      // Add other dependencies as needed
-    ],
-  },
 })
